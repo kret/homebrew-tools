1a86defb187bd37b2e1dc95b474a101465c6a644
Andrzej Undzillo <kret@users.noreply.github.com>
2021-03-15 21:49:53 +0100

    haskell-language-server-full 1.0.0 (new formula) (#2)

--- Casks/haskell-language-server-full.rb
+++ Casks/haskell-language-server-full.rb
@@ -1,11 +1,23 @@
-cask "haskell-language-server" do
+cask "haskell-language-server-full" do
   version "1.0.0"
   sha256 "74e7624c889c0235f0b02d7e7f164d5eb95b611d584fc8602f0b3a099b73f8be"
 
   url "https://github.com/haskell/haskell-language-server/releases/download/#{version}/haskell-language-server-macOS-#{version}.tar.gz"
-  name "Haskell Language Server"
-  desc "Integration point for ghcide and haskell-ide-engine. One IDE to rule them all."
+  name "Haskell Language Server full release"
+  desc "Integration point for ghcide and haskell-ide-engine. One IDE to rule them all"
   homepage "https://github.com/haskell/haskell-language-server"
+
+  conflicts_with cask: [
+    "kret/tools/haskell-language-server-wrapper",
+    "kret/tools/haskell-language-server-864",
+    "kret/tools/haskell-language-server-865",
+    "kret/tools/haskell-language-server-882",
+    "kret/tools/haskell-language-server-883",
+    "kret/tools/haskell-language-server-884",
+    "kret/tools/haskell-language-server-8102",
+    "kret/tools/haskell-language-server-8103",
+    "kret/tools/haskell-language-server-8104",
+  ]
 
   binary "haskell-language-server-wrapper"
   binary "haskell-language-server-8.6.4"
